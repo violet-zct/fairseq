b33beaf3ea6cdea9c516d8239f726b53f821dbbe
Chunting Zhou <chuntinz@fb.com>
2020-01-13 03:08:51 -0500

    fix

--- fairseq/tasks/document_mt.py
+++ fairseq/tasks/document_mt.py
@@ -194,7 +194,7 @@ class DocumentTranslationTask(FairseqTask):
         parser.add_argument('-c', '--context-suffix', type=str,
                             help='path to context: split.context-suffix')
         parser.add_argument('--input-form', type=str, choices=['cat', 'sep'], default='cat')
-        parser.add_argument('--context-form', type=str, choices=['doc', 'codes', 'sent'])
+        parser.add_argument('--context-form', type=str, choices=['doc', 'codes', 'sent', 'window'])
         parser.add_argument('--context-compress', type=str, default=None,
                             help='[format: 2,2,2], strides for conv layers in compression')
         parser.add_argument('--context-model-path', type=str, default=None,
